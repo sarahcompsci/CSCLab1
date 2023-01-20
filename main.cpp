3cb133d73629be4a2e9468c8f6fbf097bc450f82
sarahcompsci <123202075+sarahcompsci@users.noreply.github.com>
2023-01-20 14:40:15 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << " -------------------- " << endl;
+  cout << " Sarah Bender " << endl;
+  cout << " Computer Science 1 " << endl;
+  cout << " Lab1 " << endl;
+  cout << " January 20th, 2023 " << endl;
+  cout << " -------------------- " << endl;
+  cout << "Orange county community college" << endl;
+  cout << "lab1" << endl;
+  int x; //integer variable
+  int y; //integer variable;
+  x = 20;  
+  //cout << "content of X right after initialization: " << x << endl;
+  y = 5;
+  cout << "X + Y = " << x + y << endl;
+  //cout << "content of Y right before program exit: " << y << endl;
+
+  return 0;
+}
