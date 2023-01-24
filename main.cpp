378490e52c7753235b44b29fbee659e13e2803e4
sarahcompsci <123202075+sarahcompsci@users.noreply.github.com>
2023-01-24 11:16:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,90 @@
 #include <iostream>
 using namespace std;
 int main() {
+  cout << "Orange county community college" << endl;
+  cout << "lab1" << endl;
+  int x; //integer variable
+  int y; //integer variable;
+  x = 20;  
+  y = 5;
+  cout << "X + Y = " << x + y << endl;
+
+  return 0;
+}
+
+//What is the output of the program?
+//Orange county community college
+//lab1
+//x + y = 25
+
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Orange county community college" << endl;
+  cout << "lab1" << endl;
+  int x; //integer variable
+  int y; //integer variable;
+  x = 20  
+  y = 5;
+  cout << "X + Y = " << x + y << endl;
+
+  return 0;
+}
+
+//Did it compile? If not what kind of error did you get?
+//No. The error stated "expected ';' after expression x = 20
+
+//Did it compile with a semicolon?
+//Yes.
+
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Orange county community college" << endl;
+  cout << "lab1" << endl;
+  int x; //integer variable
+  int y; //integer variable;
+  x = 20;  
+  cout << "content of X right after intialization: " << x << endl;
+  y = 5;
+  cout << "X + Y = " << x + y << endl;
+  cout << "content of Y right before program exit: " << y << endl;
+
+  return 0;
+}
+
+//What kind of output did you get? 
+//Orange county community college
+//lab1
+//content of x right after initialization: 20
+//x + y = 25
+//content og y right before program exit: 5
+
+//Were you able to see the values of x and y during the program execution?
+//Yes.
+
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Orange county community college" << endl;
+  cout << "lab1" << endl;
+  int x; //integer variable
+  int y; //integer variable;
+  x = 20;  
+  //cout << "content of X right after initialization: " << x << endl;
+  y = 5;
+  cout << "X + Y = " << x + y << endl;
+  //cout << "content of Y right before program exit: " << y << endl;
+
+  return 0;
+}
+
+//Did the contents of x and y display during the run of the program?
+//No.
+
+#include <iostream>
+using namespace std;
+int main() {
   cout << " -------------------- " << endl;
   cout << " Sarah Bender " << endl;
   cout << " Computer Science 1 " << endl;
